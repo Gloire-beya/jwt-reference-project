89a01ff35baced65431b7ecda5237c418ba473e8
Gloire Beya <gloweb78@gmail.com>
2022-12-02 09:41:31 +0200

    First commit

---
+++ src/main/java/com/glory/jwtreferenceproject/JwtReferenceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.glory.jwtreferenceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtReferenceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtReferenceProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glory/jwtreferenceproject/JwtReferenceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glory.jwtreferenceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtReferenceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
